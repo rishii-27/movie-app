63232eb44d3460062aefff4b7903e7e9e83f1ca5
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2023-10-18 17:38:56 +0530

    on API fail retrying API call after every 5 secounds with cancel Retry btn

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [retrying, setRetrying] = useState(false);
+  const [retryIntervalId, setRetryIntervalId] = useState(null);
 
   const fetchMovieHandler = async () => {
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://swapi.dev/api/film/");
       if (!response.ok) {
         throw new Error("Failed to fetch data from the SWAPI");
       }
@@ -37,6 +39,18 @@ function App() {
     setIsLoading(false);
   };
 
+  function handleCancelRetryClick() {
+    setRetrying(false);
+    if (retryIntervalId) {
+      // Clear the interval using clearInterval
+      clearInterval(retryIntervalId);
+    }
+  }
+  function handleRetryClick() {
+    setRetrying(true);
+    setRetryIntervalId(setInterval(() => fetchMovieHandler(), 5000));
+  }
+
   let content = <p>No Movies found</p>;
 
   if (movies.length > 0) {
@@ -44,7 +58,20 @@ function App() {
   }
 
   if (error) {
-    content = <p>{error}</p>;
+    content = (
+      <div>
+        <p>{error}</p>
+        {retrying ? (
+          <button onClick={handleCancelRetryClick} className="btn btn-danger">
+            Cancel Retry
+          </button>
+        ) : (
+          <button onClick={handleRetryClick} className="btn btn-primary">
+            Retry
+          </button>
+        )}{" "}
+      </div>
+    );
   }
 
   if (isLoading) {
